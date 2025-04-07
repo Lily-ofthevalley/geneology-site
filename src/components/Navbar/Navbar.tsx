eef64a874f80101d0edd74f78373432ae14aa357
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-04-07 14:16:21 +0200

    finished current navbar
    Will add more pages later

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,34 @@
+import Search from "./Search";
+import { NavLink } from "react-router";
+
 const Navbar = () => {
-  return <div className="w-full p-3 bg-black">Kaas</div>;
+  return (
+    <div className="w-full bg-blue-900 flex flex-row">
+      <div className="w-7/12 flex flex-row ml-4">
+        <NavLink
+          to="/"
+          className="flex items-center justify-center w-1/5 hover:bg-blue-800 transition duration-300 py-3 px-6 rounded-lg text-white"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/familytree"
+          className="flex items-center justify-center w-1/5 hover:bg-blue-800 transition duration-300 py-3 px-6 rounded-lg text-white"
+        >
+          FamilyTree
+        </NavLink>
+        <NavLink
+          to="/graves"
+          className="flex items-center justify-center w-1/5 hover:bg-blue-800 transition duration-300 py-3 px-6 rounded-lg text-white"
+        >
+          Graves
+        </NavLink>
+      </div>
+      <div className="w-5/12 flex items-center">
+        <Search />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ src/components/Navbar/Search.tsx
@@ -0,0 +1,10 @@
+const Search = () => {
+  return (
+    <input
+      className="w-2/4 bg-white m-4 p-2 rounded-xl ml-auto"
+      placeholder="Search"
+    />
+  );
+};
+
+export default Search;
