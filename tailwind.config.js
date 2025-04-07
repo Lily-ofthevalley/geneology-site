2a785b349149ded7b4711821ae309678daec8858
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-04-07 13:49:51 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+      },
+      animation: {
+        pulse: "pulse 2s infinite",
+      },
+      keyframes: {
+        pulse: {
+          "0%, 100%": { opacity: 1 },
+          "50%": { opacity: 0.5 },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
