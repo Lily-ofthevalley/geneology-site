a17ae2de1432f98cd63884ef00651ee9faa1d608
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-04-13 12:07:20 +0200

    changed navbar colours to indigo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,23 +3,23 @@ import { NavLink } from "react-router";
 
 const Navbar = () => {
   return (
-    <div className="w-full bg-blue-900 flex flex-row">
+    <div className="w-full bg-indigo-300 flex flex-row">
       <div className="w-7/12 flex flex-row ml-4">
         <NavLink
           to="/"
-          className="flex items-center justify-center w-1/5 hover:bg-blue-800 transition duration-300 py-3 px-6 rounded-lg text-white"
+          className="flex items-center justify-center w-1/5 hover:bg-indigo-400 transition duration-300 py-3 px-6 rounded-lg text-white"
         >
           Home
         </NavLink>
         <NavLink
           to="/familytree"
-          className="flex items-center justify-center w-1/5 hover:bg-blue-800 transition duration-300 py-3 px-6 rounded-lg text-white"
+          className="flex items-center justify-center w-1/5 hover:bg-indigo-400 transition duration-300 py-3 px-6 rounded-lg text-white"
         >
           FamilyTree
         </NavLink>
         <NavLink
           to="/graves"
-          className="flex items-center justify-center w-1/5 hover:bg-blue-800 transition duration-300 py-3 px-6 rounded-lg text-white"
+          className="flex items-center justify-center w-1/5 hover:bg-indigo-400 transition duration-300 py-3 px-6 rounded-lg text-white"
         >
           Graves
         </NavLink>
